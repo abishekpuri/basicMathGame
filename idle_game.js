ff53f98080c638c1a1ba0c7e257a6bd27e4d3967
abishekpuri <apuri@connect.ust.hk>
2014-08-17 04:11:58 +0800

    Added Questions and shifted images

--- idle_game.js
+++ idle_game.js
@@ -1,18 +1,19 @@
 
 var knowledge_counter = 0;
 var level = 1;
-var additionAnswer = 0;
+var additionanswer = 0;
 window.onload = function knowledge_update(){
 document.getElementById('Knowledge').innerHTML="Knowledge Level : "+knowledge_counter;
+additionQuestion();
 }
 function knowledge_update(){
 document.getElementById('Knowledge').innerHTML="Knowledge Level : "+knowledge_counter;
 }	
 function changer() {
 	
-	images = ['Addition.gif','Multiplication.gif',]
+	images = ['img/Addition.gif','img/Multiplication.gif',]
 	title = ['Addition','Multiplication']
-	required_knowledge = [0,10]
+	required_knowledge = [0,200]
 	position = parseInt(required_knowledge.indexOf(knowledge_counter))
 	if(position!=-1){
 		document.getElementById('MathProgressImg').src = images[position];
@@ -21,19 +22,31 @@ function changer() {
 		level +=1;
 	}
 	}
-function increaseKnowledge(){
-	knowledge_counter +=1;
+function increaseKnowledge(increase){
+	knowledge_counter += increase;
 	knowledge_update();
 	document.getElementById('progressbar').value=knowledge_counter;
 	changer();
+	}
 
-}
+
 function additionQuestion(){
 number1 = Math.floor(Math.random()*100);
 number2 = Math.floor(Math.random()*100);
 /*Finish the onclick part and think about either a) adding new answer checking function or b)writing code into onclick */
-question = number1+" + "+number2+" = "+"<input type='text' id='additionAnswer'/><input type='submit' onclick=''";
+question = number1+" + "+number2+" = "+"<input type='text' id='additionanswer'/>";
 document.getElementById('addition').innerHTML = question;
-additionAnswer = number1 + number2;
+additionanswer = number1 + number2;
 }
+
+function additionAnswer(){
+if(parseInt(document.getElementById('additionanswer').value)==additionanswer){
+	increaseKnowledge(5);
+	additionQuestion();
+}
+else {
+	additionQuestion();
+}
+}
+
 
